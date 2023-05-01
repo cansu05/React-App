68614e1ca04e0898459afd7be319bf4a24ed2b70
cansu <cansuque5@gmail.com>
2023-05-01 14:15:47 +0300

    state,form

--- src/components/Products/NewProduct/ProductForm.jsx
+++ src/components/Products/NewProduct/ProductForm.jsx
@@ -1,64 +1,49 @@
 import { useState } from "react";
 import "./ProductForm.css";
 
-const ProductForm = () => {
-  // ** çoklu state kullanmı
-  const [productName, setProductName] = useState();
-  const [productPrice, setProductPrice] = useState();
-  const [imageUrl, setImageUrl] = useState();
-
-  //   const [productData, setProductData] = useState({
-  //     productName: "",
-  //     productPrice: "",
-  //     imageUrl: "",
-  //   });
+const ProductForm = (props) => {
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const titleChangeHandler = (event) => {
-    setProductName(event.target.value); //**çoklu state kullanmı
-    // **çoklu state'i tekli state şeklinde kullanma
-    // setProductData({
-    //   ...productData,
-    //   productName: event.target.value,
-    // });
-    // setProductData((prevState) => {
-    //   return { ...prevState, productName: event.target.value };
-    // });
+    setProductName(event.target.value);
   };
 
   const priceChangeHandler = (event) => {
-    setProductPrice(event.target.value); //**çoklu state kullanmı
-    // **çoklu state'i tekli state şeklinde kullanma
-    // setProductData({
-    //   ...productData,
-    //   productPrice: event.target.value,
-    // });
-    // setProductData((prevState) => {
-    //   return { ...prevState, productPrice: event.target.value };
-    // });
+    setProductPrice(event.target.value);
   };
 
   const imageChangeHandler = (event) => {
-    setImageUrl(event.target.value); //**çoklu state kullanmı
-    // **çoklu state'i tekli state şeklinde kullanma
-    // setProductData({
-    //   ...productData,
-    //   imageUrl: event.target.value,
-    // });
-    // setProductData((prevState) => {
-    //   return { ...prevState, imageUrl: event.target.value };
-    // });
+    setImageUrl(event.target.value);
   };
 
-  console.log(productData);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newProductData = {
+      productName,
+      productPrice,
+      imageUrl,
+    };
+    props.setProducts((prevState) => [...prevState, newProductData]);
+
+    console.log(newProductData);
+    setProductName("");
+    setProductPrice("");
+    setImageUrl("");
+  };
+
+  
 
   return (
-    <form className="product-form">
+    <form className="product-form" onSubmit={submitHandler}>
       <div className="product-form-input">
         <label>Ürün Adı</label>
         <input
           type="text"
           placeholder="Ürün Adı Giriniz..."
           onChange={titleChangeHandler}
+          value={productName}
         />
       </div>
       <div className="product-form-input">
@@ -67,6 +52,7 @@ const ProductForm = () => {
           type="number"
           placeholder="Ürün Fiyatı Giriniz..."
           onChange={priceChangeHandler}
+          value={productPrice}
         />
       </div>
       <div className="product-form-input">
@@ -75,6 +61,7 @@ const ProductForm = () => {
           type="text"
           placeholder="Ürün Görseli Giriniz..."
           onChange={imageChangeHandler}
+          value={imageUrl}
         />
       </div>
       <button className="product-form-button">Ürün Ekle</button>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,18 @@ import ProductItem from "./ProductItem";
 import { productData } from "../../productData";
 import "./Products.css";
 import NewProduct from "./NewProduct/NewProduct";
+import { useState } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState(productData);
+
+
   return (
     <div className="product-wrapper">
-      <NewProduct />
+      <NewProduct setProducts={setProducts}/>
       <h1>Products</h1>
       <div className="products">
-        {productData.map((product) => {
+        {products.map((product) => {
           return <ProductItem key={product.productName} product={product} />;
         })}
       </div>
